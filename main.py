6f7042af9ef4b8e637a5d8d9a8639bd3db27b9bd
akumarm23 <140874386+akumarm23@users.noreply.github.com>
2023-11-21 20:25:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,98 @@
-#Password Generator Project
-import random
-import string
+# #Password Generator Project
+# import random
+# import string
 
-# Create a variable named letters containing both lowercase and uppercase letters
-letters = list(string.ascii_lowercase + string.ascii_uppercase)
+# # Create a variable named letters containing both lowercase and uppercase letters
+# letters = list(string.ascii_lowercase + string.ascii_uppercase)
 
-# Create a variable named numbers containing numbers from 0 to 9
-numbers = list(map(str, range(10)))
+# # Create a variable named numbers containing numbers from 0 to 9
+# numbers = list(map(str, range(10)))
 
-# Create a variable named symbols containing common symbols
-#symbols = list(string.punctuation)
-symbols = ['!','@','#','$','%','&','+','*','(',')']
+# # Create a variable named symbols containing common symbols
+# #symbols = list(string.punctuation)
+# symbols = ['!','@','#','$','%','&','+','*','(',')']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+# print("Welcome to the PyPassword Generator!")
+# nr_letters = int(input("How many letters would you like in your password?\n")) 
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 
-#Eazy Level
-# password = ""
+# #Eazy Level
+# # password = ""
+
+# # for char in range(1, nr_letters + 1):
+# #   password += random.choice(letters)
+
+# # for char in range(1, nr_symbols + 1):
+# #   password += random.choice(symbols)
+
+# # for char in range(1, nr_numbers + 1):
+# #   password += random.choice(numbers)
+
+# # print(password)
+
+# #Hard Level
+# password_list = []
+
 
 # for char in range(1, nr_letters + 1):
-#   password += random.choice(letters)
+#   password_list.append(random.choice(letters))
 
 # for char in range(1, nr_symbols + 1):
-#   password += random.choice(symbols)
+#   password_list += random.choice(symbols)
 
 # for char in range(1, nr_numbers + 1):
-#   password += random.choice(numbers)
+#   password_list += random.choice(numbers)
 
-# print(password)
+# #print(password_list)
+# random.shuffle(password_list)
+# #print(password_list)
 
-#Hard Level
+# password = ""
+# for char in password_list:
+#   password += char
+
+# print(f"Your password is: {password}")
+
+# # END OF CODE
+
+
+
+# Version 2.0
+
+import random
+import string
+import sys
+
+letters = list(string.ascii_lowercase + string.ascii_uppercase)
+numbers = list(map(str, range(10)))
+symbols = ['!', '@', '#', '$', '%', '&', '+', '*', '(', ')']
+
+if len(sys.argv) != 4:
+    print("Usage: python main.py <number_of_letters> <number_of_symbols> <number_of_numbers>")
+    sys.exit(1)
+
+nr_letters = int(sys.argv[1])
+nr_symbols = int(sys.argv[2])
+nr_numbers = int(sys.argv[3])
+
 password_list = []
 
-
 for char in range(1, nr_letters + 1):
-  password_list.append(random.choice(letters))
+    password_list.append(random.choice(letters))
 
 for char in range(1, nr_symbols + 1):
-  password_list += random.choice(symbols)
+    password_list += random.choice(symbols)
 
 for char in range(1, nr_numbers + 1):
-  password_list += random.choice(numbers)
+    password_list += random.choice(numbers)
 
-#print(password_list)
 random.shuffle(password_list)
-#print(password_list)
 
 password = ""
 for char in password_list:
-  password += char
+    password += char
 
 print(f"Your password is: {password}")
 
-# END OF CODE
